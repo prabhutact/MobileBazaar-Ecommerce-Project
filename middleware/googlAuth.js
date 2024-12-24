f87ca5d4b434a863a95e88cec53e4f8da92ac05f
prabhutact <prabhutact@gmail.com>
2024-12-24 16:14:49 +0530

    code updated on 24-12-2024

--- middleware/googlAuth.js
+++ middleware/googlAuth.js
@@ -2,8 +2,8 @@
 const passport = require('passport')
 const GoogleStrategy = require('passport-google-oauth2').Strategy;
 passport.use(new GoogleStrategy({
-    clientID: "758178201442-ur27dq0eomlshke2bjea3pod6ad156r2.apps.googleusercontent.com",
-    clientSecret: "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",
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
     callbackURL:  "/auth/google/callback",
     passReqToCallback: true
 },
